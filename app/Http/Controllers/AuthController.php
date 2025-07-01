91cb8da86e5985007f41feb793dc8aabc9f4ab0d
paiva <lucas.paiva@ae3tecnologia.com.br>
2025-06-30 21:19:25 -0400

    criando funcao de registro do usuario

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,23 +4,41 @@ namespace App\Http\Controllers;
 
 use App\Base\Http\Controllers\BaseController;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
 use App\Services\Auth\LoginService;
+use App\Services\Auth\RegisterService;
 use Exception;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
+use Illuminate\Http\JsonResponse;
 
 class AuthController extends BaseController
 {
+
+    /**
+     * @param RegisterService $registerService
+     * @param RegisterRequest $data
+     * @return JsonResponse
+     */
+    public function register(RegisterService $registerService, RegisterRequest $data) {
+        try {
+            return self::successResponse(
+                data: $registerService->execute($data->validated()),
+                message: 'Login realizado com sucesso!'
+            );
+        } catch (Exception $exception) {
+            return self::returnError($exception);
+        }
+    }
+
     /**
      * @param LoginService $loginService
-     * @param LoginRequest $loginRequest
-     * @return \Illuminate\Http\JsonResponse|\Symfony\Component\HttpFoundation\Response
+     * @param LoginRequest $data
+     * @return JsonResponse|
      */
-    public function login(LoginService $loginService, LoginRequest $loginRequest)
+    public function login(LoginService $loginService, LoginRequest $data)
     {
         try {
             return self::successResponse(
-                data: $loginService->execute($loginRequest->validated()),
+                data: $loginService->execute($data->validated()),
                 message: 'Login realizado com sucesso!'
             );
         } catch (Exception $exception) {
