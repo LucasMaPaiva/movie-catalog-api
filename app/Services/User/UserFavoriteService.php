3fdfa4be04213136fc8ac83ad01b808c19b07ffb
paiva <lucas.paiva@ae3tecnologia.com.br>
2025-07-01 16:27:34 -0400

    criando service para salvar o filme favorito

---
+++ app/Services/User/UserFavoriteService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Services\User;
+
+use App\Models\User;
+use App\Repositories\FavoritesRepository;
+use App\Repositories\UserRepository;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+class UserFavoriteService
+{
+
+    /**
+     * @param int $id
+     * @param array $data
+     * @return mixed
+     */
+    public function execute(int $id, array $data)
+    {
+       return app(FavoritesRepository::class)->save([
+           'movie_id' => $data['movie_id'],
+           'user_id' => $id,
+           'movie_details' => $data['movie_details'],
+       ]);
+    }
+}
