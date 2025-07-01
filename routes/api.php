6d6275fa56fc4eb97666d545fe9b89d08990f929
paiva <lucas.paiva@ae3tecnologia.com.br>
2025-06-30 20:21:03 -0400

    criando rota de login

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MovieController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1')->group(function () {
+
+    Route::prefix('auth')->group(function () {
+        Route::post('login', [AuthController::class, 'login']);
+    });
+
+    Route::get('movies', [MovieController::class, 'show']);
+});
